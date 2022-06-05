e0e2f2714b135cfc52c850d45e0f0ba884fceff1
Sourav Ghosh <sourav@consilioussystems.com>
2022-06-05 09:34:38 +0530

    update migration

--- www/hr-service/database/migrations/2022_06_03_125145_create_personalities_types_table.php
+++ www/hr-service/database/migrations/2022_06_03_125145_create_personalities_types_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personality_types', function (Blueprint $table) {
+        Schema::create('personalities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personality_types');
+        Schema::dropIfExists('personalities');
     }
 };
